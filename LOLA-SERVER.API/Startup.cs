d8a1c7d13bf726dc672a7af63285759aac38717a
Sebastian Castellanos <castellanospsebastian@gmail.com>
2024-05-13 17:24:28 -0500

    add startup

---
+++ LOLA-SERVER.API/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace LOLA_SERVER.API
+{
+    public class Startup(IConfiguration configuration)
+    {
+        public IConfiguration Configuration { get; } = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors("AllowAll");
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+    }
+}
